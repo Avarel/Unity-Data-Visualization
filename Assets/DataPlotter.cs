095febf58c1e9bcf220998b43f9c179f0db91dea
Avarel <avarelpm@gmail.com>
2020-01-28 22:53:12 -0800

    Plot the data

--- Assets/DataPlotter.cs
+++ Assets/DataPlotter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class DataPlotter : MonoBehaviour
 {
@@ -8,14 +9,31 @@ public class DataPlotter : MonoBehaviour
     // Name of the input file, no extension
     public string inputfile;
 
-
     // List for holding data from CSV reader
     private List<Dictionary<string, object>> pointList;
+
+    // Scale of the plot
+    public float plotScale = 10;
+
+    // Indices for columns to be assigned
+    public int columnX = 0;
+    public int columnY = 1;
+    public int columnZ = 2;
+
+    // Full column names
+    public string xName;
+    public string yName;
+    public string zName;
+
+    // The prefab for the data points that will be instantiated
+    public GameObject PointPrefab;
+
+    // The holder of our points
+    public GameObject PointHolder;
 
     // Use this for initialization
     void Start()
     {
-
         // Set pointlist to results of function Reader with argument inputfile
         pointList = CSVReader.Read(inputfile);
 
@@ -26,10 +44,72 @@ public class DataPlotter : MonoBehaviour
         List<string> columnList = new List<string>(pointList[1].Keys);
 
         // Print number of keys (using .count)
-        Debug.Log("There are " + columnList.Count + " columns in CSV");
+        Debug.Log("There are " + columnList.Count + " columns in the CSV");
 
         foreach (string key in columnList)
             Debug.Log("Column name is " + key);
+
+        // Assign column name from columnList to Name variables
+        xName = columnList[columnX];
+        yName = columnList[columnY];
+        zName = columnList[columnZ];
+
+        // Get maxes of each axis
+        float xMax = FindMaxValue(xName);
+        float yMax = FindMaxValue(yName);
+        float zMax = FindMaxValue(zName);
+
+        // Get minimums of each axis
+        float xMin = FindMinValue(xName);
+        float yMin = FindMinValue(yName);
+        float zMin = FindMinValue(zName);
+
+        //Loop through Pointlist
+        for (var i = 0; i < pointList.Count; i++)
+        {
+            // Get value in poinList at ith "row", in "column" Name, normalize
+            float x = (Convert.ToSingle(pointList[i][xName]) - xMin) / (xMax - xMin);
+
+            float y = (Convert.ToSingle(pointList[i][yName]) - yMin) / (yMax - yMin);
+
+            float z = (Convert.ToSingle(pointList[i][zName]) - zMin) / (zMax - zMin);
+
+            //instantiate the prefab with coordinates defined above
+            GameObject dataPoint = Instantiate(PointPrefab, new Vector3(x, y, z) * plotScale, Quaternion.identity);
+            dataPoint.GetComponent<Renderer>().material.color = new Color(x, y, z, 1.0f);
+            dataPoint.transform.parent = PointHolder.transform;
+        }
+
     }
 
+    private float FindMaxValue(string columnName)
+    {
+        //set initial value to first value
+        float maxValue = Convert.ToSingle(pointList[0][columnName]);
+
+        //Loop through Dictionary, overwrite existing maxValue if new value is larger
+        for (var i = 1; i < pointList.Count; i++)
+        {
+            if (maxValue < Convert.ToSingle(pointList[i][columnName]))
+                maxValue = Convert.ToSingle(pointList[i][columnName]);
+        }
+
+        //Spit out the max value
+        return maxValue;
+    }
+
+    private float FindMinValue(string columnName)
+    {
+
+        float minValue = Convert.ToSingle(pointList[0][columnName]);
+
+        //Loop through Dictionary, overwrite existing minValue if new value is smaller
+        for (var i = 1; i < pointList.Count; i++)
+        {
+            if (Convert.ToSingle(pointList[i][columnName]) < minValue)
+                minValue = Convert.ToSingle(pointList[i][columnName]);
+        }
+
+        return minValue;
+    }
 }
